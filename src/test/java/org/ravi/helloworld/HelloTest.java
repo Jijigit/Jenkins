f988492a9984faecd7187255b158611f97408409
Jiji <jiji.unnikrishnan@accenture.com>
2018-05-31 11:24:27 +0530

    new code added

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,8 +19,8 @@ public class HelloTest {
 	@BeforeTest
 	public void beforeTest() {
 		
-		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.firefox.marionette","resources\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		driver.get("http://demoaut.com");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 		
@@ -31,7 +31,7 @@ public class HelloTest {
 
 		String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
 		System.out.println("The date is: "+text1);
-		driver.findElement(By.name("userName")).sendKeys("vishnu");
+		driver.findElement(By.name("userName")).sendKeys("remi");
 		String text = driver.findElement(By.name("userName")).getAttribute("value");
 		System.out.println("The Username entered is: "+text);
 
